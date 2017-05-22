4f409be835c161c231c2e0f2751eccea914faa55
joooong <jol5599@gmail.com>
2017-05-22 17:48:08 +0900

    request, inviteRequest interface

---
+++ ordermade/src/ordermade/store/logic/RequestStoreLogic.java
@@ -0,0 +1,14 @@
+package ordermade.store.logic;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RequestStoreLogic {
+	
+	public RequestStoreLogic() {
+		// TODO Auto-generated constructor stub
+	}
+	
+//	public(){}
+
+}
