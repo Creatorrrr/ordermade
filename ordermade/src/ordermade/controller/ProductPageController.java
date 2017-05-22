2b528723a440005018e3065cf4faa381ae1c395d
csi <ckthdlr@naver.com>
2017-05-22 12:40:09 +0900

    spring setting

--- ordermade/src/ordermade/controller/ProductPageController.java
+++ ordermade/src/ordermade/controller/ProductPageController.java
@@ -1,8 +1,23 @@
 package ordermade.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ordermade.domain.ProductPage;
+import ordermade.service.facade.ProductPageService;
 
 @Controller
 public class ProductPageController {
-
+	
+	@Autowired
+	private ProductPageService ppService;
+	
+	@RequestMapping(value="/productpage/register.do", method=RequestMethod.GET)
+	public String registerProductPage(ProductPage productPage, HttpSession session) {
+		return "";
+	}
 }
