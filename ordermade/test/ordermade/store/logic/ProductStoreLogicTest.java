50c3a59eab6c32df37e8067db84ede1037feef20
csi <ckthdlr@naver.com>
2017-05-23 11:49:59 +0900

    .

---
+++ ordermade/test/ordermade/store/logic/ProductStoreLogicTest.java
@@ -0,0 +1,79 @@
+package ordermade.store.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/WEB-INF/dispatcher-servlet.xml"})
+public class ProductStoreLogicTest {
+
+	@Test
+	public void testInsertProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateProductById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteProductById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategoryOrderByHitsForMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategoryOrderByIdForMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategoryAndTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategoryAndMakerName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByCategoryAndMakerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByMakerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProductsByMakerIdAndTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectAllCategory() {
+		fail("Not yet implemented");
+	}
+
+}
