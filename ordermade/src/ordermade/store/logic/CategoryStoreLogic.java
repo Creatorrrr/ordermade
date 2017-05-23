0531173d17bd1765c736375d12b60bc6bad60ff4
eomjaeun <flutter.redien@gmail.com>
2017-05-23 11:46:18 +0900

    ss

--- ordermade/src/ordermade/store/logic/CategoryStoreLogic.java
+++ ordermade/src/ordermade/store/logic/CategoryStoreLogic.java
@@ -2,15 +2,24 @@ package ordermade.store.logic;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
 import ordermade.domain.Category;
 import ordermade.store.facade.CategoryStore;
+import ordermade.store.mapper.CategoryMapper;
+import ordermade.store.mapper.TagMapper;
 
 public class CategoryStoreLogic implements CategoryStore {
 
 	@Override
 	public List<Category> selectAllCategory() {
-		// TODO Auto-generated method stub
-		return null;
+//		SqlSession session = OrderMadeSessionFactory.getInstance().getSession();
+
+//		CategoryMapper mapper = session.getMapper(TagMapper.class);
+//		check = mapper.insertTag(tag);
+//		session.close();
+//
+//		return check > 0;
 	}
 
 }
