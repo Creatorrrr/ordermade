a048833ab2fbe90b290e5617c08cb064a1ceb3fd
eomjaeun <flutter.redien@gmail.com>
2017-05-24 17:35:27 +0900

    Love Love Love 5월 24일

--- ordermade/src/ordermade/store/logic/MemberStoreLogic.java
+++ ordermade/src/ordermade/store/logic/MemberStoreLogic.java
@@ -1,5 +1,6 @@
 package ordermade.store.logic;
 
+import org.apache.ibatis.executor.ExecutorException;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
@@ -9,42 +10,81 @@ import ordermade.store.mapper.MemberMapper;
 
 @Repository
 public class MemberStoreLogic implements MemberStore {
-	
-	private SqlSession session;
-	
-	public MemberStoreLogic() {
-		 session = SqlSessionFactoryProvider.getSqlSessionFactory().openSession();
-	}
 
 	@Override
 	public boolean insertMember(Member member) {
-		MemberMapper mapper = session.getMapper(MemberMapper.class);
-		int check = mapper.insertMember(member);
-		session.close();
-		return check > 0;
+
+		SqlSession session = SqlSessionFactoryProvider.getSqlSessionFactory().openSession();
+
+		boolean result = false;
+
+		try {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+
+			if (result = mapper.insertMember(member) > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return result;
 	}
 
 	@Override
 	public boolean updateMemberById(Member member) {
-		MemberMapper mapper = session.getMapper(MemberMapper.class);
-		int check = mapper.updateMemberById(member);
-		session.close();
-		return check > 0;
+
+		SqlSession session = SqlSessionFactoryProvider.getSqlSessionFactory().openSession();
+
+		boolean result = false;
+		try {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+
+			if (result = mapper.updateMemberById(member) > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return result;
 	}
 
 	@Override
 	public boolean deleteMemberById(String id) {
-		MemberMapper mapper = session.getMapper(MemberMapper.class);
-		int check = mapper.deleteMemberById(id);
-		session.close();
-		return check > 0;
+
+		SqlSession session = SqlSessionFactoryProvider.getSqlSessionFactory().openSession();
+
+		boolean result = false;
+		try {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+			if (result = mapper.deleteMemberById(id) > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return result;
 	}
 
 	@Override
 	public Member selectMemberById(String id) {
-		MemberMapper mapper = session.getMapper(MemberMapper.class);
-		Member member = mapper.selectMemberById(id);
-		session.close();
+
+		SqlSession session = SqlSessionFactoryProvider.getSqlSessionFactory().openSession();
+
+		Member member = null;
+		try {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+			member = mapper.selectMemberById(id);
+		} catch (ExecutorException e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 		return member;
 	}
 
